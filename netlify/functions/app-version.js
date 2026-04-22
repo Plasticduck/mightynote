dc45a3deb9f12867477af5185ac00ee8f2447bc1
Plasticduck <your-email@example.com>
2026-04-22 14:50:14 -0500

    Remove GL code placeholder text; bump version to 26

--- netlify/functions/app-version.js
+++ netlify/functions/app-version.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
     // Return the current app version
     // Update this number whenever you deploy a new version
-    const CURRENT_VERSION = 25;
+    const CURRENT_VERSION = 26;
     
     return {
         statusCode: 200,
